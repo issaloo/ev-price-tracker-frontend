05b3d06e0e433068ba36746eff5f3bcca8e7c8e1
Issac Loo <issaloo@umich.edu>
2023-10-18 21:27:30 -0400

    feat(multiple): update

--- app/graphs/page.tsx
+++ app/graphs/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <p>Graphs</p>
+      <p>Graphs Okay!</p>
     </div>
   );
 }
