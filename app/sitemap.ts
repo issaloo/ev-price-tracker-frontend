87f7b5c42cca0915037dd751d91a9d66ead6ae0b
Issac Loo <issaloo@umich.edu>
2023-11-16 21:01:20 -0500

    feat(multiple): add robots and sitemap dynamically

---
+++ app/sitemap.ts
@@ -0,0 +1,33 @@
+import { getEvPrice } from "./hooks/getEvPrice";
+import { EvPrice } from "./types/evPrice";
+import { EvPriceDetail } from "./types/evPriceDetail";
+
+export default async function sitemap() {
+  const URL = "https://www.evpricetracker.com";
+  const evPriceJson = await getEvPrice();
+  const curDate = new Date().toISOString();
+
+  // get dynamic graph routes
+  const graphs = evPriceJson.map((ev: EvPrice) => {
+    const brand = ev.brandName;
+    return ev.itemDetails.map((item: EvPriceDetail) => {
+      return {
+        url: `${URL}/graph/${brand}-${item.modelName.replace(/\s+/g, "-")}`,
+        lastModified: curDate,
+        priority: 0.8,
+      };
+    });
+  });
+  const graphsFlat = graphs.flat(1);
+
+  // get static routes
+  const routes = ["", "about", "faq", "privacy", "disclaimer", "vehicles"].map(
+    (route) => ({
+      url: `${URL}/${route}`,
+      lastModified: curDate,
+      priority: route === "" ? 1 : 0.5,
+    }),
+  );
+
+  return [...routes, ...graphsFlat];
+}
